c051900363a51cc9db00d01695259bb234d25116
HappyMatDuckling <matveyd05@yandex.ru>
2024-10-10 18:50:17 +0300

    I finished 0 track

--- Ya_Lyceum/0-8/letter_soup.go
+++ Ya_Lyceum/0-8/letter_soup.go
@@ -1,1 +1,30 @@
 package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func Permutations(input string) []string {
+	if len(input) == 1 {
+		return []string{input}
+	}
+
+	var lsPermutations []string
+	for ind, letter := range input {
+		letterStr := string(letter)
+		var inpWithoutLetter = input[:ind] + input[ind+1:]
+		var supPerm = Permutations(inpWithoutLetter)
+		for _, el := range supPerm {
+			lsPermutations = append(lsPermutations, letterStr+el)
+		}
+	}
+	sort.Strings(lsPermutations)
+	return lsPermutations
+}
+
+func main() {
+	str := "cab"
+	permutations := Permutations(str)
+	fmt.Println("Permutations:", permutations)
+}
